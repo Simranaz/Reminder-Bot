563d71e7b544e7213758f73175ea6c9730d39a2b
Simra Naz <152845560+Simranaz@users.noreply.github.com>
2025-02-26 21:35:42 +0530

    Add files via upload

---
+++ spambot.py
@@ -0,0 +1,35 @@
+import pyautogui, time, datetime
+  
+time.sleep(2) 
+  
+while True: 
+    # to display the time at which the message is sent  
+    print(datetime.datetime.now()) 
+    pyautogui.typewrite("Reminder: Drink water!")  
+    pyautogui.press("enter") 
+    time.sleep(1) 
+  
+    print(datetime.datetime.now()) 
+  
+    pyautogui.typewrite("Reminder: Reminder: Take medicine!")
+    pyautogui.press("enter") 
+    time.sleep(2) 
+  
+    print(datetime.datetime.now()) 
+  
+    pyautogui.typewrite("Reminder: Take the dog for a walk!") 
+    pyautogui.press("enter") 
+    time.sleep(3) 
+  
+    print(datetime.datetime.now()) 
+  
+    pyautogui.typewrite("Reminder: Drink water!") 
+    pyautogui.press("enter") 
+    time.sleep(4) 
+  
+    print(datetime.datetime.now()) 
+  
+    pyautogui.typewrite("Reminder: Drink water!") 
+    pyautogui.press("enter") 
+    time.sleep(5) 
+    
